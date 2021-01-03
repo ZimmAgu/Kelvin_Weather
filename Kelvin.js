dfcadd9b0c79bf487ba5ec21c6d8ab3f995cb904
Zim Agu <zimmagu@gmail.com>
2021-01-03 13:00:45 -0800

    Update Kelvin.js
    Finished the Project

--- Kelvin.js
+++ Kelvin.js
@@ -1,19 +1,42 @@
 
-// KELVIN is now a constat variable
-// it will always be set to 293
-const KELVIN = 293;
+// KELVIN is now a constant variable
+// it will always be set to 0
+const KELVIN = 0;
 
 
-// CELSIUS is now a constat variable
+// CELSIUS is now a constant variable
 // it will always be set to 273 degrees less than KELVIN
 const CELSIUS =  KELVIN - 273;
 
 
-//This is the formula for fahrenheight
+
+
+
+
+// This is the formula for fahrenheight
 let Fahrenheight = CELSIUS * (9/5) + 32;
 
 
-//Now the fahrenheight will be rounded to the nearest whole number
-Fahrenheight = Math.floor(Fahrenheight);
+// Defining newton
+let Newton = CELSIUS * (33/100);
 
-console.log("The temperature is ");
+
+
+
+
+
+// Now the fahrenheight & Newton will be rounded to the nearest whole number
+Fahrenheight = Math.floor(Fahrenheight);
+Newton = Math.floor(Newton);
+
+
+
+
+
+
+
+
+// Output in Fahrenheight & Newton
+console.log(`The temperature is ${Fahrenheight} degrees fahrenheight`);
+
+console.log(`The temperature is ${Fahrenheight} degrees fahrenheight`);
