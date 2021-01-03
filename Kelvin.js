6c629ae2517b33b905e5b8b8e0c5fa5fd480378d
Zim Agu <zimmagu@gmail.com>
2021-01-03 10:58:43 -0800

    Create Kelvin.js
    Defined Kelving, Celsius, & Fahrenheight

---
+++ Kelvin.js
@@ -0,0 +1,13 @@
+
+// KELVIN is now a constat variable
+// it will always be set to 293
+const KELVIN = 293;
+
+
+// CELSIUS is now a constat variable
+// it will always be set to 273 degrees less than KELVIN
+const CELSIUS =  KELVIN - 273;
+
+
+//This is the formula for fahrenheight
+let Fahrenheight = CELSIUS * (9/5) + 32;
